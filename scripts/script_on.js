e1ad951335789f8134f065a2142f8d2ef47129f8
SendMeCatPics <kapigakatherine@gmail.com>
2024-12-04 21:51:27 -0500

    first commit

---
+++ scripts/script_on.js
@@ -0,0 +1,39 @@
+fetch('https://temp.staticsave.com/674ff929adb04.json')
+.then(response => response.json())
+.then(databaseON => {
+    databaseON["Ontario Vehicle Information"].Owner_Details.forEach(element => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${element.Registration_ID_Number}</td>
+            <td>${element.Name}</td>
+            <td>${element.Contact_Number}</td>
+            <td>${element.Province_ID}</td>
+            <td>${element.Postal_Code}</td>`
+
+        document.getElementById("OwnersA").appendChild(row);
+    });
+    databaseON["Ontario Vehicle Information"].Vehicle_Information.forEach(element => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${element.Province_ID}</td>
+            <td>${element.Registration_ID_Number}</td>
+            <td>${element.Vehicle_Information_Number}</td>
+            <td>${element.Date_Acquired}</td>
+            <td>${element.Brand}</td>
+            <td>${element.Model}</td>`
+
+        document.getElementById("VehiclesA").appendChild(row);
+    });
+    databaseON["Ontario Vehicle Information"].Theft_Reports.forEach(element => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${element.Date_Stolen}</td>
+            <td>${element.Registration_ID_Number}</td>
+            <td>${element.Vehicle_Information_Number}</td>
+            <td>${element.Location}</td>
+            <td>${element.Province_ID}</td>
+            <td>${element.Status}</td>`
+
+        document.getElementById("TheftsA").appendChild(row);
+    });
+});
---
+++ scripts/script_qc.js
@@ -0,0 +1,49 @@
+fetch('https://temp.staticsave.com/674f9e8ec04b3.xml')
+.then(response => response.text())
+.then(databaseQC => {
+        let parser = new DOMParser(),
+        xml = parser.parseFromString(databaseQC, "text/xml");
+
+        let owners = xml.getElementsByTagName("Owner");
+        let i;
+        let tableOwners = "";
+        for (i = 0; i < owners.length; i++) {
+            tableOwners += "<tr><td>" +
+            owners[i].getElementsByTagName("Registration_ID_Number")[0].childNodes[0].nodeValue + "</td><td>" + 
+            owners[i].getElementsByTagName("Name")[0].childNodes[0].nodeValue + "</td><td>" + 
+            owners[i].getElementsByTagName("Contact_Number")[0].childNodes[0].nodeValue + "</td><td>" + 
+            owners[i].getElementsByTagName("Province_ID")[0].childNodes[0].nodeValue + "</td><td>" + 
+            owners[i].getElementsByTagName("Postal_Code")[0].childNodes[0].nodeValue + "</td></tr>";
+        }
+        document.getElementById("OwnersB").innerHTML = tableOwners;
+
+        let vehicles = xml.getElementsByTagName("Vehicle");
+        let j;
+        let tableVehicles = "";
+        for (j = 0; j < vehicles.length; j++) {
+            tableVehicles += "<tr><td>" +
+            vehicles[j].getElementsByTagName("Vehicle_Information_Number")[0].childNodes[0].nodeValue + "</td><td>" + 
+            vehicles[j].getElementsByTagName("Registration_ID_Number")[0].childNodes[0].nodeValue + "</td><td>" + 
+            vehicles[j].getElementsByTagName("Province_ID")[0].childNodes[0].nodeValue + "</td><td>" + 
+            vehicles[j].getElementsByTagName("Brand")[0].childNodes[0].nodeValue + "</td><td>" + 
+            vehicles[j].getElementsByTagName("Model")[0].childNodes[0].nodeValue + "</td><td>" + 
+            vehicles[j].getElementsByTagName("Date_Acquired")[0].childNodes[0].nodeValue + "</td></tr>";
+        }
+        document.getElementById("VehiclesB").innerHTML = tableVehicles;
+
+        let reports = xml.getElementsByTagName("Report");
+        console.log(reports)
+        let k;
+        let tableReports = "";
+        for (k = 0; k < reports.length; k++) {
+            tableReports += "<tr><td>" +
+            reports[k].getElementsByTagName("Date_Stolen")[0].childNodes[0].nodeValue + "</td><td>" + 
+            reports[k].getElementsByTagName("Registration_ID_Number")[0].childNodes[0].nodeValue + "</td><td>" + 
+            reports[k].getElementsByTagName("Vehicle_Information_Number")[0].childNodes[0].nodeValue + "</td><td>" + 
+            reports[k].getElementsByTagName("Location")[0].childNodes[0].nodeValue + "</td><td>" + 
+            reports[k].getElementsByTagName("Province_ID")[0].childNodes[0].nodeValue + "</td><td>" + 
+            reports[k].getElementsByTagName("Status")[0].childNodes[0].nodeValue + "</td></tr>";
+        }
+        document.getElementById("TheftsB").innerHTML = tableReports;
+
+});
